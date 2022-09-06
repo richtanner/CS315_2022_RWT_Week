fc35a3f6a10b7f658bbada220396691b5220ce75
Rich Tanner <rwt07a@acu.edu>
2022-09-06 08:17:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/rwtweek1application/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.rwtweek1application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.RWTWeek1Application.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intentWhatever = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intentWhatever.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intentWhatever);
+    }
+
+    public void aboutMe(View view) {
+        Intent thirdIntent = new Intent(this, ThirdActivity.class);
+        startActivity(thirdIntent);
+    }
+}
